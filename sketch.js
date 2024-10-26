2e711db77cc44da7e12c87cac96f3e02734e571d
hlq <justinjoslin@proton.me>
2024-10-26 18:30:47 -0500

    full board rendering w/slider

--- sketch.js
+++ sketch.js
@@ -2,9 +2,19 @@ let displaySize = 900;
 let cellSize = 10;
 let timeStep = 1; // time between frames in seconds;
 let dims = displaySize/cellSize;
+let timeLayers = 10
 
-let currentTimeStep = Math.floor(dims/2)
-let frameSlider;
+let currentTimeStep = 0;
+let layerSlider;
+let layerSliderText;
+
+let timeLayersAmount;
+let timeLayersAmountText;
+
+let displayAll = false;
+let displayAllCheckbox;
+
+let paused = false;
 
 /*
 [
@@ -20,7 +30,7 @@ let gameBoard = (()=>{
   for(var i = 0; i < dims; i++){
     arr[i] = new Array(dims);
     for(var j = 0; j < dims; j++){
-      arr[i][j] = new Array(dims);
+      arr[i][j] = new Array(timeLayers);
     }
   }
   return arr;
@@ -31,7 +41,7 @@ let gameBoard = (()=>{
 function stepTime(){
   for(var y = 0; y < dims; y++){
     for(var x = 0; x < dims; x++){
-      gameBoard[y][x].push(new Array(dims));
+      gameBoard[y][x].push(new Array(timeLayers));
       gameBoard[y][x].shift();
     }
   }
@@ -52,17 +62,173 @@ class Cell {
   }
 }
 
+function gradient(startColor, endColor, steps, position) {
+  if (position < 0 || position >= steps) {
+    throw new Error("Position must be within the range of steps");
+  }
 
+  // Calculate the step increments for each RGB channel
+  const stepR = (endColor[0] - startColor[0]) / (steps - 1);
+  const stepG = (endColor[1] - startColor[1]) / (steps - 1);
+  const stepB = (endColor[2] - startColor[2]) / (steps - 1);
+
+  // Calculate color at the specified position
+  const r = Math.round(startColor[0] + stepR * position);
+  const g = Math.round(startColor[1] + stepG * position);
+  const b = Math.round(startColor[2] + stepB * position);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+// input, odds must be larger than what Math.random returns in order for a cell to be placed
+function randomCells(odds){
+  for(var y = 0; y < dims; y++){
+    for(var x = 0; x < dims; x++){
+      for(var z = 0; z < timeLayers; z++){
+        if(odds > Math.random()){
+          gameBoard[y][x][z] = new Cell(true, gradient([255,0,0], [0, 255, 255], timeLayers, currentTimeStep));
+        } else {
+          gameBoard[y][x][z] = new Cell(false, 255, timeLayers, currentTimeStep)
+        }
+      }
+    }
+  }
+}
+
+function renderBoard(){
+  for(var y = 0; y < dims; y++){
+    for(var x = 0; x < dims; x++){
+      var snappedX = cellSize * x;
+      var snappedY = cellSize * y;
+      if(displayAll){
+        for(var z = 0; z < dims; z++){
+          testCell = gameBoard[y][x][currentTimeStep-1]
+          if(testCell.livingState){
+            fill(gradient([255,0,0], [255, 0, 77], dims, z))
+            rect(snappedX, snappedY, cellSize, cellSize);
+          }
+        }
+      }else{
+        testCell = gameBoard[y][x][currentTimeStep-1]
+        if(testCell.livingState){
+          //console.log(x, y, currentTimeStep, gameBoard[y][x][currentTimeStep])
+          fill(gradient([255,0,0], [255, 0, 77], dims, currentTimeStep))
+          rect(snappedX, snappedY, cellSize, cellSize);
+        }  else {
+          //console.log(x, y, currentTimeStep, gameBoard[y][x][currentTimeStep])
+          fill(255)
+          rect(snappedX, snappedY, cellSize, cellSize);
+        }
+      }
+    }
+  }
+}
 
 function setup() {
   createCanvas(displaySize, displaySize);
-
+  console.log(dims)
   frameRate(timeStep);
-  frameSlider = createSlider(0, 90)
-  frameSlider.position(700, 100)
+  layerSlider = createSlider(1, 10)
+  layerSlider.value(0)
+  layerSlider.position(700, 100)
+  layerSliderText = createInput()
+  layerSliderText.size(25)
+  layerSliderText.position(700, 80)
+  layerSliderText.value(layerSlider.value())
+
+  timeLayersAmount = createSlider(10, 10)
+  timeLayersAmount.position(700, 150)
+  timeLayersAmount.value(10)
+  timeLayersAmountText = createInput()
+  timeLayersAmountText.size(25)
+  timeLayersAmountText.position(700, 130)
+  timeLayersAmountText.value(timeLayersAmount.value())
+
+  console.log(gameBoard[0])
+  displayAllCheckbox = createCheckbox();
+  displayAllCheckbox.position(700, 200);
+  randomCells(0.5)
+  // console.log(gameBoard)
 }
 
 function draw() {
-  background(220);
-  // shiftTime();
+  background(255);
+  displayAll = displayAllCheckbox.checked();
+  layerSliderText.input(updateViewLayerSlider);
+  layerSlider.input(updateViewLayerTextBox);
+  currentTimeStep = layerSlider.value();
+  timeLayersAmountText.input(updateTimeLayerSlider);
+  timeLayersAmount.input(updateTimeLayerTextBox);
+  timeLayers = timeLayersAmount.value()
+  renderBoard()
+  // stepTime();
+}
+
+function updateViewLayerSlider(){
+  let inputVal = layerSliderText.value()
+  let valCheck = inputVal.charCodeAt(inputVal.length -1)
+  if(inputVal.charCodeAt(0) == 48) {
+    layerSliderText.value(inputVal.substring(1))
+  }
+
+  // if it isn't a number, delete it
+  if(!(valCheck > 47 && valCheck < 58)) {
+    layerSliderText.value(inputVal.substring(0, inputVal.length-1))
+  }
+  //if it's greater than 90, it isn't
+  if(inputVal > 90) {
+    layerSlider.value(90)
+    layerSliderText.value(90)
+  } else {
+    layerSlider.value(layerSliderText.value())
+  }
+  inputVal = layerSliderText.value()
+  if(inputVal == "") {
+    layerSliderText.value(0)
+    layerSlider.value(layerSliderText.value())
+  }
+}
+
+function updateViewLayerTextBox(){
+  layerSliderText.value(layerSlider.value())
+}
+
+function updateTimeLayerSlider(){
+  let inputVal = timeLayersAmountText.value()
+  let valCheck = inputVal.charCodeAt(inputVal.length -1)
+  if(inputVal.charCodeAt(0) == 48) {
+    timeLayersAmountText.value(inputVal.substring(1))
+  }
+
+  // if it isn't a number, delete it
+  if(!(valCheck > 47 && valCheck < 58)) {
+    timeLayersAmountText.value(inputVal.substring(0, inputVal.length-1))
+  }
+  //if it's greater than 90, it isn't
+  if(inputVal > 90) {
+    timeLayersAmount.value(90)
+    timeLayersAmountText.value(90)
+  } else {
+    timeLayersAmount.value(timeLayersAmountText.value())
+  }
+  inputVal = timeLayersAmountText.value()
+  if(inputVal == "") {
+    timeLayersAmountText.value(0)
+    timeLayersAmount.value(timeLayersAmountText.value())
+  }
+}
+
+function updateTimeLayerTextBox(){
+  timeLayersAmountText.value(timeLayersAmount.value())
+}
+
+function keyPressed(e){
+  if(keyCode == 32){
+    paused = !paused;
+    if(paused){
+      noLoop();
+    }else{
+      loop();
+    }
+  }
 }
